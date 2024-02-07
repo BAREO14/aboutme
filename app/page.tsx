f9356cdfb95b47128c2dfd0ec328d0f892da8022
Bright Esosa Ogiesoba <157101723+BAREO14@users.noreply.github.com>
2024-02-07 12:45:21 +0000

    🐱‍🏍Updated About ✔

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,10 @@ export default function Home() {
     <>
    <h1>I am <b>Bright Esosa Ogiesoba</b></h1>
    <p>I am a Programmer who loves coding</p>
-   <p>I love drinking H<sub>2</sub>O and there is usually a bottle of H<sub>2</sub>O on my desk anytime ni'm coing.  [O<sup>2</sup> is also very compulsory in the life of any programmer]
+   <p>I love <mark>drinking</mark> H<sub>2</sub>O and there is <i>usually</i> a bottle of H<sub>2</sub>O on my desk anytime i'm coing.  O<sup>2</sup> is also very compulsory in the life of any programmer
   </p>
   <h2>Apart from <u>coding</u>, there are other things I
-    enjoy doing and they include writing songs,
-    frestyling and spending time with <mark>friends</mark>.
-  </h2>
-  
-    <p>I'm tring to stay focused and acqquire this 
-      skill and do away with <s>distractions</s>.It's not going to be <i>easy</i>, but God dey.
-    </p>
-    <p>One more thing, I <s>deleted</s> the app I created and started over from 
-      comm. prompt up to three times before I could <b>Finally</b> get this done.
-    </p>
-    <p>But it feels so good finally being able to do this, thanks to our captain, miss Tosin.</p>
+    frestyling and <u>spending</u> time with <mark>friends</mark></h2>
   </>
   );
 }
