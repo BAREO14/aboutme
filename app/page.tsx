e7c92f5dd667263abf518ce768352bd5ad51d486
Bright Esosa Ogiesoba <157101723+BAREO14@users.noreply.github.com>
2024-02-12 18:27:26 +0100

    Update page.tsx🪁

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
    <h1>I am <b>Bright Esosa Ogiesoba</b></h1>
    <p>I am a Programmer who loves coding</p>
-   <p>I love <mark>drinking</mark> H<sub>2</sub>O and there is <i>usually</i> a bottle of H<sub>2</sub>O on my desk anytime i'm coing.  O<sup>2</sup> is also very compulsory in the life of any programmer
+   <p>I love <mark>drinking</mark> H<sub>2</sub>O and there is <i>usually</i> a bottle of H<sub>2</sub>O on my desk anytime i&apos;m coing.  O<sup>2</sup> is also very compulsory in the life of any programmer
   </p>
   <h2>Apart from <u>coding</u>, there are other things I
     frestyling and <u>spending</u> time with <mark>friends</mark></h2>
